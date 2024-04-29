2be2c2514ce4264f1cffb682e9e8870a340fff64
jundachen10 <jundachen10@gmail.com>
2024-04-29 15:34:44 -0400

    set virtual enviroment and install pandas

---
+++ src/extract.py
@@ -0,0 +1,14 @@
+import csv
+import pandas as pd
+
+# Create a DataFrame from a Python dictionary
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
