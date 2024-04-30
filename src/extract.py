8809556c4fb97f10defdcec59521f204347e55d9
jundachen10 <jundachen10@gmail.com>
2024-04-30 14:08:49 -0400

    :recycle: refactor: in extract.py we removed the script file path logic because it is now in main.py

--- src/extract.py
+++ src/extract.py
@@ -2,26 +2,20 @@ import pandas as pd
 import os
 
 def extract_data_from_csv(file_path):
+    """
+    Reads a CSV file into a pandas DataFrame.
+    
+    Parameters:
+    - file_path (str): The full path to the CSV file.
+    
+    Returns:
+    - pandas.DataFrame: The data from the CSV file, or None if an error occurs.
+    """
     try:
         # Load the CSV data into a DataFrame
         data = pd.read_csv(file_path)
-
-        # Display the first few rows of the DataFrame
-        print("First few rows of the CSV file:")
-        print(data.head())
-
-        # Optional: Perform additional processing here
-        return data #this is the frame object called data
+        return data
     
     except Exception as e:
         print(f"An error occurred while reading the CSV file: {e}")
         return None
-
-if __name__ == "__main__":
-    # Construct the file path relative to the script location
-    current_dir = os.path.dirname(__file__)  # Gets the directory where the script is located
-    project_root = os.path.dirname(current_dir)  # Moves up to the project root directory
-    file_path = os.path.join(project_root, 'data', 'test_data.csv')  # Constructs the path to the CSV
-    
-    # Call the function to extract data
-    data = extract_data_from_csv(file_path)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from extract import extract_data_from_csv
 
 def main():
+    
     current_dir = os.path.dirname(__file__)
     project_root = os.path.dirname(current_dir)
     file_path = os.path.join(project_root, 'data', 'test_data.csv')
